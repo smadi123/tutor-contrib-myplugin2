5ff4125d25cb56540b8eadfe0a0a4f0d2a08ff52
smadi <smadi123@gmail.com>
2024-07-21 14:32:03 +0400

    Test Tutor Plugin Cookiecutter

---
+++ README.rst
@@ -0,0 +1,23 @@
+myplugin2 plugin for `Tutor <https://docs.tutor.edly.io>`__
+###############################################################################
+
+Testing Tutor plugin cookiecutter
+
+
+Installation
+************
+
+.. code-block:: bash
+
+    pip install git+https://github.com/smadi123/tutor-contrib-myplugin2
+
+Usage
+*****
+
+.. code-block:: bash
+
+    tutor plugins enable myplugin2
+License
+*******
+
+This software is licensed under the terms of the AGPLv3.
---
+++ setup.py
@@ -0,0 +1,68 @@
+import io
+import os
+
+from setuptools import find_packages, setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_readme():
+    with io.open(os.path.join(HERE, "README.rst"), "rt", encoding="utf8") as f:
+        return f.read()
+
+
+def load_about():
+    about = {}
+    with io.open(
+        os.path.join(HERE, "tutormyplugin2", "__about__.py"),
+        "rt",
+        encoding="utf-8",
+    ) as f:
+        exec(f.read(), about)  # pylint: disable=exec-used
+    return about
+
+
+ABOUT = load_about()
+
+
+setup(
+    name="tutor-contrib-myplugin2",
+    version=ABOUT["__version__"],
+    url="https://github.com/smadi123/tutor-contrib-myplugin2",
+    project_urls={
+        "Code": "https://github.com/smadi123/tutor-contrib-myplugin2",
+        "Issue tracker": "https://github.com/smadi123/tutor-contrib-myplugin2/issues",
+    },
+    license="AGPLv3",
+    author="Amjad Smadi",
+    author_email="smadi123@gmail.com",
+    description="Testing Tutor plugin cookiecutter",
+    long_description=load_readme(),
+    long_description_content_type="text/x-rst",
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=["tutor>=18.0.0,<19.0.0"],
+    extras_require={
+        "dev": [
+            "tutor[dev]>=18.0.0,<19.0.0",
+        ]
+    },
+    entry_points={
+        "tutor.plugin.v1": [
+            "myplugin2 = tutormyplugin2.plugin"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
